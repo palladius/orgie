56117aab329b81e3ef3cd3a2a0ef3479110fdd9b
Riccardo <rusko@palladius.it>
2011-01-08 09:46:50 +0000

    added empty rails 3 project :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orgie::Application.config.secret_token = '28b3114ac8d78b88fbca243a7704d546622c68cf76eeb7390ac89ba20082c6704000772605723f77ed5e1f23ff8835336181537b69a035ff82d70d283e2d3e20'
